391b64870c47e00b6dd1a51a116d1aa92eba912f
Sohanuzzaman3301 <86237901+Sohanuzzaman3301@users.noreply.github.com>
2022-05-10 03:11:05 +0000

    Add files via upload

---
+++ theme-script.sh
@@ -0,0 +1,16 @@
+cd ~
+git clone https://github.com/yeyushengfan258/ArcStarry-Cursors.git
+cd ~/ArcStarry-Cursors
+./install.sh
+
+
+git clone https://github.com/vinceliuice/WhiteSur-gtk-theme
+cd ~/WhiteSur-gtk-theme
+sudo ./install.sh -t purple -c dark -p smaller -i fedora  -m -s 180
+
+
+git clone https://github.com/vinceliuice/Tela-icon-theme
+cd ~/Tela-icon-theme
+./install.sh -c purple
+
+
